4346d0c981895a438e439d5a90db156f54f23080
Sofiane <belmeh_s@etna-alternance.net>
2020-07-22 02:38:55 +0200

    correction des droit sur les routes

--- miscroserviceApp/geteway/src/main/java/com/microservice/gateway/security/SecurityTokenConfig.java
+++ miscroserviceApp/geteway/src/main/java/com/microservice/gateway/security/SecurityTokenConfig.java
@@ -33,7 +33,7 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 		// authorization requests config
 		.authorizeRequests()
 		   // allow all who are accessing "auth" service
-		   .antMatchers("/auth/**", "/commun/**, /crud/**").permitAll()  
+		   .antMatchers("/auth/**", "/commun/**", "/crud/**").permitAll()  
 		   // must be an admin if trying to access admin area (authentication is also required here)
 		   .antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
 		   // Any other request must be authenticated
